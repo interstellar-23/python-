9c22ef471953cb6bc1e3e66a419e326dc01a1571
interstellar-23 <467478578@qq.com>
2023-09-18 00:05:36 +0800

    九九乘法表

---
+++ 99乘法表.py
@@ -0,0 +1,10 @@
+
+for a in range(1,10):
+    for b in range(1,a+1):
+        print("%d*%d=%d\t"%(a,b,a*b),end="\t")
+        if b==a:
+            print(end="\n")
+        
+        
+        
+    
